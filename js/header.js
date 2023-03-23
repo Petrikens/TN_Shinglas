4eba020e811e7d65364a028959921a8762eb1035
Petrikens <ppvs96@mail.ru>
2023-03-23 19:41:35 +0300

    добавил появление всплывающего окна по ховеру

--- js/header.js
+++ js/header.js
@@ -1,6 +1,8 @@
 let menuBtn = document.querySelector(".burger_menu");
 let menu = document.querySelector(".mobile_menu");
 let menuItem = document.querySelectorAll(".navbar_item");
+let galleryItem = document.querySelectorAll(".gallery");
+let collectionMenu = document.querySelector(".collection_menu");
 
 menuBtn.addEventListener("click", function () {
   menuBtn.classList.toggle("active");
@@ -12,4 +14,20 @@ menuItem.forEach(function (menuItem) {
     menuBtn.classList.toggle("active");
     menu.classList.toggle("active");
   });
+});
+
+galleryItem.forEach(function (galleryItem) {
+  galleryItem.addEventListener("mouseover", function () {
+    collectionMenu.classList.add("active_collection_menu");
+  });
+  galleryItem.addEventListener("mouseout", function () {
+    collectionMenu.classList.remove("active_collection_menu");
+  });
+});
+
+collectionMenu.addEventListener("mouseover", function () {
+  collectionMenu.classList.add("active_collection_menu");
+});
+collectionMenu.addEventListener("mouseout", function () {
+  collectionMenu.classList.remove("active_collection_menu");
 });
