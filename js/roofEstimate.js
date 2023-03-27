ca8c92190d45e9fd028064210ae1e5d13d4b1a36
Petrikens <ppvs96@mail.ru>
2023-03-27 17:06:49 +0300

    добавил слайдер в roofEstimate section

--- js/roofEstimate.js
+++ js/roofEstimate.js
@@ -1,4 +1,5 @@
-const estimateCards = document.querySelectorAll(".roof_card");
+const roofEstimateSlider = document.getElementById("roof_estimate_slider");
+const estimateCards = roofEstimateSlider.querySelectorAll(".slider_card");
 
 const markEstimateCard = (cardIndex) => {
   estimateCards.forEach((card, index) => {
@@ -30,4 +31,77 @@ estimateCards.forEach((card, index) => {
   card.addEventListener("click", function () {
     markEstimateCard(index);
   });
+});
+
+class EstimateSlider {
+  constructor(slider) {
+    this.slider = slider;
+    this.sliderItems = this.slider.querySelector(".slider_items");
+    this.sliderTrack = this.slider.querySelector(".slider_track");
+    this.slides = this.slider.querySelectorAll(".slider_card");
+    this.arrows = this.slider.querySelector(".slider_menu");
+    this.prev = this.slider.querySelector(".prev");
+    this.next = this.slider.querySelector(".next");
+    this.slideWidth = this.slides[0].offsetWidth;
+    this.slideIndex = 0;
+    this.maxIndex =
+      this.slides.length -
+      Math.floor(this.slider.offsetWidth / this.slideWidth);
+  }
+
+  createSliderItems() {
+    for (let i = 0; i <= this.maxIndex; i++) {
+      let li = document.createElement("li");
+      li.classList.add("slider_item");
+      if (i === 0) {
+        li.classList.add("active_item");
+      }
+      this.sliderItems.appendChild(li);
+    }
+  }
+
+  slide() {
+    this.sliderTrack.style.transition = "transform .5s";
+
+    this.sliderTrack.style.transform = `translate3d(-${
+      this.slideIndex * this.slideWidth
+    }px, 0px, 0px)`;
+  }
+
+  activeItem(slideIndex) {
+    this.sliderItems.querySelectorAll(".slider_item").forEach((item, index) => {
+      item.classList.remove("active_item");
+      if (index === slideIndex) {
+        item.classList.add("active_item");
+      }
+    });
+  }
+}
+
+const estimateSlider = new EstimateSlider(roofEstimateSlider);
+
+estimateSlider.createSliderItems();
+
+estimateSlider.arrows.addEventListener("click", function (event) {
+  let target = event.target;
+
+  if (target.classList.contains("next")) {
+    estimateSlider.slideIndex++;
+    estimateSlider.activeItem(estimateSlider.slideIndex);
+    if (estimateSlider.slideIndex > estimateSlider.maxIndex) {
+      estimateSlider.slideIndex = 0;
+      estimateSlider.activeItem(estimateSlider.slideIndex);
+    }
+  } else if (target.classList.contains("prev")) {
+    if (estimateSlider.slideIndex > 0) {
+      estimateSlider.slideIndex--;
+      estimateSlider.activeItem(estimateSlider.slideIndex);
+    } else if (estimateSlider.slideIndex === 0) {
+      estimateSlider.slideIndex = estimateSlider.maxIndex;
+      estimateSlider.activeItem(estimateSlider.slideIndex);
+    }
+  } else {
+    return;
+  }
+  estimateSlider.slide();
 });
